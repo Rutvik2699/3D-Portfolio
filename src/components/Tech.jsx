4dc9903dd5ac66423a07b2ed53c2ca1a8189e349
Rutvik2699 <rutvikbhoj2611@gmail.com>
2023-06-14 09:54:40 -0400

    First Commit

---
+++ src/components/Tech.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react-refresh/only-export-components */
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+const Tech = () => {
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-12">
+      {technologies.map((technology) => (
+        <div
+          className="w-28 h-28 rounded-2xl flex-col align-center content-center black-gradient
+          transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:border duration-300 text-center 
+          "
+          key={technology.name}
+        >
+          <img className="p-3 " src={technology.icon} />
+
+          <span>{technology.name}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SectionWrapper(Tech, "");
---
+++ src/constants/index.js
@@ -0,0 +1,294 @@
+import {
+    mobile,
+    backend,
+    
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+   
+    airpods,
+   bigboytoys,interacts,
+    threejs,cognizant,mit,ghd,conestoga,CV_Duncan
+  } from "../assets";
+  
+
+export const CVDuncan = CV_Duncan
+
+
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+    {
+      id:"resume",
+      title:"Download Resume",
+    
+    }
+  ];
+  
+  const services = [
+    {
+      title: "Full Stack Web Developer",
+      icon: web,
+    },
+    {
+      title: "React  Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "Figma",
+      icon: figma,
+    },
+    // {
+    //   name: "docker",
+    //   icon: docker,
+    // },
+  ];
+  
+  const experiences = [
+    {
+      title: "Bachelor's of Computer Science",
+      company_name: "MIT-WPU",
+      icon: mit,
+      iconBg: "#383E56",
+      date: "April 2017 - May 2020",
+      points: [
+        "Learned all the fundamentals of Computer Science from System design to Databse structure.",
+        "Studied in detail about programming languages like C, C++, Java, JavaScript, Python and hands on practical as well.",
+        "Along with theoretical knowledge, gained practical skills in critical thinking, problem solving, and analysis of the given problems.",
+        "Developed multiple real world projects in the last year of my degree, such as Car reseller website, real time weather app and ecommerce webapp.",
+      ],
+    },
+    {
+      title: "Programmer Analyst",
+      company_name: "Cognizant",
+      icon: cognizant,
+      iconBg: "#E6DEDD",
+      date: "Nov 2020 - Dec 2021",
+      points: [
+        "Developed reusable and modular components in React, resulting in reduction in development time, successfully integrated RESTfull APIs into REACT application using libraries such as Axios and Fetch.",
+        "Built responsive and mobile-first user interfaces using CSS frameworks resulting in an improvement in user engagement.",
+        "Collaborated with designers and other developers to deliver high-quality front-end solutions and achieved a client satisfaction rate of 90%.",
+        "Debugged and resolved issues in existing applications and boosted performance. Utilized modern development tools such as Git, Node.js, Webpack, and Babel to streamline the development process, resulting  increase in productivity.",
+      ],
+    },
+    {
+      title: "Software Development Engineer in Test - COOP",
+      company_name: "GHD Pvt Ltd",
+      icon: ghd,
+      iconBg: "#E6DEDD",
+      date: "Sept 2022 - Dec 2022",
+      points: [
+        "Developed and implemented automated test scripts using Cypress JavaScript framework and Selenium which reduced testing time, and collaborating with development teams to identify and fix software defects promptly, resulting in 30% reduction of code defects.",
+        "Utilized agile testing methodologies to prioritize and execute test cases, I have achieved a 25% increase in testing efficiency and increased application performance.",
+        "Created Test Cases, Test Scenarios and performed them on the websites. Presented results with developers and the client.",
+        "Conducted comprehensive end-to-end testing to ensure the quality and stability of the software, I have achieved a 90% reduction in post-release defects. Also managed defects and tickets with Azure DevOps.",
+      ],
+    },
+    {
+      title: "Mobile Solutions Development - Post Graduation",
+      company_name: "Conestoga College",
+      icon: conestoga,
+      iconBg: "#383E56",
+      date: "Jan 2022 - April 2023",
+      points: [
+        "Learned about system design and development, its concepts and analysis of software development.",
+        "Developed wireframes and create UI's in Adobe Creative Suite.",
+        "Developed user friendly apps for iOS and Android mobile operating system along with Full Stack Web Applications.",
+        "Developed a Real Time Chat application as my capstone project to connect international student before arriving in Canada.",
+      ],
+    },
+    
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+
+  const myWords = [
+    {
+      text: "Here you can download my Resume by clicking on Download button. I would be glad to connect with you.",
+      name: "Rutvik Bhoj",
+      // designation: "The owner",
+      // company: "",
+      // image: me,
+    },
+  ]
+
+
+  const projects = [
+    {
+      name: "Airpods",
+      description:
+        "Replicated Apple Airpods page using basic HTML, CSS, JavaScript to showcase my UI Skills.",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "JavaScript",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: airpods,
+      source_code_link: "https://github.com/Rutvik2699/Airpods",
+      live_link:"https://rutvik2699.github.io/Airpods/"
+    },
+    {
+      name: "Car Showcase",
+      description:
+        "Car Showcase website to demonstrate Frontend Skills.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "JavaScript",
+          color: "green-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: bigboytoys,
+      source_code_link: "https://github.com/Rutvik2699/Big-Boy-Toys",
+      live_link:"https://rutvik2699.github.io/Big-Boy-Toys/"
+    },
+    {
+      name: "Interacts",
+      description:
+        "A Real Time Chat application for International Students to interacts with eachother and dicuss college ideas together.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "javascript",
+          color: "green-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "chakraUI",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "socket.io",
+          color: "green-text-gradient",
+        },
+        {
+          name: "crud",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: interacts,
+      source_code_link: "https://github.com/CInteracts/Interacts",
+      live_link:"https://github.com/CInteracts/Interacts"
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects,myWords };
